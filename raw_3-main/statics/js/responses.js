e243b756a107f8daf1518366e1d6ade02bc0ea80
AkashSingh20 <akashsingh20603@gmail.com>
2023-04-06 22:31:23 +0530

    some

--- raw_3-main/statics/js/responses.js
+++ raw_3-main/statics/js/responses.js
@@ -16,6 +16,14 @@ function getBotResponse(input) {
         return "rock";
     }
 
+    if (input == "tell me about website") {
+        return "As remote and hybrid working models become more prevalent, virtual meetings have become necessary. Meetings are often held through platforms like Microsoft Teams, Google Meet, and Zoom. Although these platforms offer the ability to download transcripts, they cannot summarize them. In order to address this gap in meeting productivity, the project aims to create a solution that provides a summary of virtual meetings.";
+    } else if (input == "tell me the objective of the website") {
+        return "kaam karna lodu";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
 
 
     // Simple responses
